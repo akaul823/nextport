1c829c6597da99baf4e3399aabf08e47ef60d29d
avi kaul <avikarkaul@gmail.com>
2024-01-18 03:02:29 -0500

    got contact header, ned skill header

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -33,9 +33,9 @@ function Contact() {
     transition={{
         duration: 1.5
     }}
-    className='h-screen flex relative flex-col text-center md:text-left md:flex-row 
-    max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-20 md:top-24 uppercase tracking-[10px] md:tracking-[20px] text-lg md:text-2xl text-rose-800'>
+    className='min-h-screen flex overflow-hidden flex-col text-left
+    max-w-full px-10 justify-evenly mx-auto items-center'>
+        <h3 className='top-20 md:top-24 uppercase tracking-[10px] md:tracking-[20px] text-lg md:text-2xl text-rose-800'>
             Contact
         </h3>
         <div className='flex flex-col space-y-10'>
