c2e33b8557f7e67a30b34e1290acf6dd19d759eb
avi kaul <avikarkaul@gmail.com>
2024-01-19 16:33:30 -0500

    yuh

--- src/app/components/HomeButton.jsx
+++ src/app/components/HomeButton.jsx
@@ -9,7 +9,6 @@ function HomeButton() {
     <footer className='sticky bottom-4 w-full cursor-pointer'>
       <div className='flex items-center justify-center'>
         <Image width={200} height={200} alt='home-button' className="h-14 w-14 rounded-full filter ml-[-80%] md:ml-[-90%] grayscale hover:grayscale-0 cursor-pointer" src="/images/coolLogo.png" />
-
       </div>
     </footer>
   </Link>
